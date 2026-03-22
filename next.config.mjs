5cd1b36dbeaee323f05a81f78e74255e3c55288e
hellomraza <hellomraza@gmail.com>
2026-03-22 13:41:51 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3000/:path*",
-      },
-    ];
-  },
-}
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "http://localhost:3000/:path*",
+  //     },
+  //   ];
+  // },
+};
 
-export default nextConfig
+export default nextConfig;
